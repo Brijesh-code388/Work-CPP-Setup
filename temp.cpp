40fd0a6e2d185b62cd98b91ef24279ba3c41c2da
Brijesh-code388 <jayswaminarayan388@gmail.com>
2022-04-22 18:22:53 +0530

    Vscode C++ CP Setup

---
+++ temp.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include <math.h>
+#define mod 1000000007
+#define ll long long int
+#define pii pair<int, int>
+using namespace std;
+
+void fio()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    freopen("./inputf.in", "r", stdin);
+    freopen("./outputf.out", "w", stdout);
+}
+int main()
+{
+    fio();
+    int t;
+    while (t--)
+    {
+    }
+}
+
+return 0;
+}
